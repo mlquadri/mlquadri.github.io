9b6a47158fce5116ed2b90e4c80a8a66efd73f6c
mlquadri <mslhquadri@gmail.com>
2019-03-18 09:58:37 -0500

    debuging

--- p5/JS/DNDnockoffExtraFunctions.js
+++ p5/JS/DNDnockoffExtraFunctions.js
@@ -32,7 +32,7 @@ function preScene_charmInputAct(){
 }
 
 function preScene_option1Act(){
-	if( (diceButtonClicked >= 4) $$ (nameEntered) ){
+	if( (diceButtonClicked >= 4) && (nameEntered) ){
 		trueList = [false, false, false, false];
 		statList = [statDefence, statStelth, statAttack, statCrisma];
 		for (var i = 0; i < 4; i++){
