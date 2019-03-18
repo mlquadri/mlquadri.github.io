c313a62a7db5d62fc6952f6086452904ef513dbd
mlquadri <mslhquadri@gmail.com>
2019-03-18 08:04:00 -0500

    sd

--- p5/JS/DNDnockoffExtraFunctions.js
+++ p5/JS/DNDnockoffExtraFunctions.js
@@ -10,7 +10,7 @@ function preScene_nameAct(){
 function preScene_diceAct(){
 	print(diceButtonClicked)
 	if(diceButtonClicked < 4){
-		diceNum = rollDice(0);
+		diceNum = rollDice(5);
 		alert(diceNum);
 		//add num to an aray
 		diceButtonClicked+=1;
